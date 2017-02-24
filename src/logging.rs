8b8f8e8a36bd8d01312dbabbcb32150f00fac598
Per Lundberg <perlun@gmail.com>
2017-02-24 20:45:26 +0200

    wip

--- src/logging.rs
+++ src/logging.rs
@@ -6,6 +6,7 @@ impl<D> Middleware<D> for Logger {
     fn invoke<'mw, 'conn>(&self, req: &mut Request<'mw, 'conn, D>, res: Response<'mw, D>)
     -> MiddlewareResult<'mw, D> {
         info!("{0} {1} {2}", res.status().to_u16(), req.origin.method, req.origin.uri);
+        res.all_on_start();
 
         res.next_middleware()
     }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,27 @@ extern crate nickel;
 extern crate log;
 
 extern crate log4rs;
-extern crate hyper;
+extern crate rustc_serialize;
 
 use nickel::{HttpRouter, Nickel, StaticFilesHandler};
+use rustc_serialize::json;
 use std::default::Default;
 use std::env;
 
 mod logging;
 
+// Automatically generate `RustcDecodable` and `RustcEncodable` trait
+// implementations
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct TestStruct  {
+    data_int: u8,
+    data_str: String,
+    data_vector: Vec<u8>,
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
+
     if args.len() < 2 {
         panic!("You must provide a folder name where the static files for the app root is located.");
     }
@@ -26,7 +37,13 @@ fn main() {
     let mut router = Nickel::router();
 
     router.get("/api/sessions", middleware! {
-        "foo"
+        let object = TestStruct {
+            data_int: 1,
+            data_str: "homura".to_string(),
+            data_vector: vec![2,3,4,5],
+        };
+
+        json::encode(&object).unwrap()
     });
     server.utilize(router);
 
